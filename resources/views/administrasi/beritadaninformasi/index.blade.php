9d116757642966423ea4cbec000bb3268ae34da6
xeizer <keiser.form@gmail.com>
2020-07-09 11:47:16 +0700

    perbaikan berita

--- resources/views/administrasi/beritadaninformasi/index.blade.php
+++ resources/views/administrasi/beritadaninformasi/index.blade.php
@@ -20,7 +20,7 @@
                       @foreach($isi as $a)
                       <tr>
                         <td>{{$loop->index+1}}</td>
-                        <td>{{$a->user->name}}</td>
+                        <td>{{$a->user->name ?? ''}}</td>
                         <td>{{$a->kategori}}</td>
                         <td>{{$a->judul}}</td>
                         <td>{{$a->konten}}</td>
